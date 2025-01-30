8c83479bf420312880c7b041258957149bb17bb6
Hugh <hughbrown123@hotmail.co.uk>
2025-01-30 06:33:58 -0500

    Updated mainClient.c and mainServer.c; sockets created and first welcome message sent from server to client.

--- client/clientMain.c
+++ client/clientMain.c
@@ -1,17 +1,25 @@
+
+
+// Need to add in how the client handles \r\n at the end of server commands. This could be its own function.
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <arpa/inet.h> // This is required for inet_addr
+#include <arpa/inet.h> // This is required for inet_pton
+#include <unistd.h> // This is required for close
 
 // Function Signatures
 void create_control_channel(int, const char*, int);
-void create_data_channel(int, const char*, int);
+void create_data_channel(int, const char*, int, struct sockaddr_in*);
+int isValidIpAddress(char *ipAddress, struct sockaddr_in* server_address);
+
 
 int main(int argc, char *argv[]) {
 
-// Command is ran by user with two arguments; target IP and port number.
+// Command is ran by user with two arguments; target IP and port number of FTP server.
 // Checking two arguments are provided.
 if (argc != 3) {
     printf("Error in syntax.\n Usage: %s <Server IP> <Target Port>\n", argv[0]);
@@ -21,32 +29,30 @@ if (argc != 3) {
 
 
 // Checking the port number inputted in valid
-const char *server_ip = argv[1];
 char *endptr;
 long port = strtol(argv[2], &endptr, 10);
-if (*endptr != '\0' || port < 0 || port > 65535) {
-    printf("Invalid port number. Port must be between 0 and 65535.\n");
+if (*endptr != '\0' || port < 1 || port > 65535) {
+    printf("Invalid port number. Port must be between 1 and 65535 inclusive.\n");
     exit(5);
 }
 int target_control_port = (int)port;
 
-// Need code to check the IP address inputted is valid.
-printf("Put in code to check the IP inputted is valid.\n");
+// Checking the IP address inputted is valid
+char *server_ip = argv[1];
+struct sockaddr_in server_address;
+if (isValidIpAddress(server_ip, &server_address) <= 0) {
+    printf("Invalid IP address.\n");
+    exit(5);
+} else if (server_ip == NULL) {
+    perror("An error has occurred regarding server IP checking.\n");
+}
 
-
-// Welcome message.
-const char client_message[] = "Welcome to the FTP client";
-
-printf("%s\n", client_message);
-
-// Creating a two socket (Control and Data), which will reach out to server later.
-// AF_INET = IPv4, SOCK_STREAM = TCP, 0 = IP
 
 // Control channel socket created, though not bound to any address yet.
 int client_control_socket;
 client_control_socket = socket(AF_INET, SOCK_STREAM, 0);
 if (client_control_socket < 0) {
-    perror("Error in creating control socket");
+    perror("Error in creating control socket.\n");
     exit(EXIT_FAILURE);
 }
 
@@ -59,23 +65,45 @@ if (client_data_socket < 0) {
     exit(EXIT_FAILURE);
 }
 
-printf("Data channel: %i, Control socket: %i\n",client_control_socket, client_data_socket); //Debugging line
+// Welcome message.
+printf("Welcome to the FTP client!\n");
+printf("\n");
+printf("Attempting to connected to FTP server at %s on port %d.\n\n", server_ip, target_control_port);
 
-return 0;
+// Creating a two socket (Control and Data), which will reach out to server later.
+// AF_INET = IPv4, SOCK_STREAM = TCP, 0 = IP
+
 
 // Connect to the control channel
 create_control_channel(client_control_socket, server_ip, target_control_port);
 
 /* Assuming target_data_port is the same as target_control_port for now
-Will need to implement logic to grab the data port from the control channel later.
+Will need to implement logic to grab the data port from the control channel later (such as when we see a PASV command etc).
 */
-int target_data_port = target_control_port;
+
+// Client will recv() welcome message from server and print it
+char server_welcome_message[256];
+int received_message_check = recv(client_control_socket, &server_welcome_message, sizeof(server_welcome_message) - 1, 0);
+if (received_message_check < 0) {
+    perror("Error receiving welcome message from server");
+    close(client_control_socket);
+    exit(EXIT_FAILURE);
+}
+server_welcome_message[received_message_check] = '\0';
+printf("%s\n", server_welcome_message);
+
+
+int target_data_port = target_control_port; // <----   This is a temporary, and outright wrong. So will remove later.
 
 // Connect to the data channel - This will need to be put inside its own FUNCTION to be called upon later when needed.
-create_data_channel(client_data_socket, server_ip, target_data_port);
+// create_data_channel(client_data_socket, server_ip, target_data_port);
+
+// Close sockets
+close(client_control_socket);
+// close(client_data_socket);
 
 return 0;
-}
+} // end of main
 
 // Function call to create and connect the control channel
 void create_control_channel(int client_control_socket, const char* server_ip, int target_control_port) {
@@ -84,30 +112,31 @@ void create_control_channel(int client_control_socket, const char* server_ip, in
     struct sockaddr_in server_address;
     server_address.sin_family = AF_INET; // This defines the address type as IPv4
     server_address.sin_port = htons(target_control_port); // This defines the port number, htons converts the port (int) to a network byte order.
-    server_address.sin_addr.s_addr = inet_addr(server_ip); // This defines the IP address of the server, inet_addr converts the IP address to a binary format.
+    server_address.sin_addr.s_addr = inet_addr(server_ip); // This defines the IP address of the server
 
     int control_connection_status = connect(client_control_socket, (struct sockaddr*) &server_address, sizeof(server_address)); // This connects the client to the server. Casting is used to change the variable type of the structure, as required by connect()
 
     // Error handling
     if (control_connection_status < 0) {
-        perror("Error in connecting to the server's control channel.");
+        perror("Error in connecting to the server's control channel. Reason: "); // error name is saved to global variable "errno", which is printed at the end of the perror() string automatically.
         exit(EXIT_FAILURE); // Note perror prints the error message to stderr, so function type can be ANYTHING.
 }
 
-printf("FAKE - Connected to the server's control channel.\n");
+    printf("Connected to the server's control channel.\n");
 }
 
+
+
 // Function call to create the data channel
-void create_data_channel(int client_data_socket, const char* server_ip, int target_data_port) {
+void create_data_channel(int client_data_socket, const char* server_ip, int target_data_port, struct sockaddr_in* server_address) {
 
     // Specify an address for the data socket
-    struct sockaddr_in server_address;
-    server_address.sin_family = AF_INET; // This defines the address type as IPv4
-    server_address.sin_port = htons(target_data_port); 
+    server_address->sin_family = AF_INET; // This defines the address type as IPv4
+    server_address->sin_port = htons(target_data_port); 
     /* IMPORTANT NOTE
     The variable target_data_port is not yet defined or captured in this program, we need to somehow grab it from the data exchanged on the control channel.
     */
-   server_address.sin_addr.s_addr = inet_addr(server_ip);
+   server_address->sin_addr.s_addr = inet_addr(server_ip);
 
    int data_connection_status = connect(client_data_socket, (struct sockaddr*) &server_address, sizeof(server_address)); // This connects the client to the server. Casting is used to change the variable type of the structure, as required by connect()
 
@@ -117,4 +146,9 @@ void create_data_channel(int client_data_socket, const char* server_ip, int targ
         exit(EXIT_FAILURE); // Note perror prints the error message to stderr, so function type can be ANYTHING.
 }
 printf("FAKE - Connected to the server's data channel.\n");
+}
+
+int isValidIpAddress(char *ipAddress, struct sockaddr_in* server_address) {
+    int result = inet_pton(AF_INET, ipAddress, &(server_address->sin_addr));
+    return result == 1;
 }
--- server/serverMain.c
+++ server/serverMain.c
@@ -5,12 +5,11 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <ctype.h>
+#include <arpa/inet.h> // This is required for inet_pton and INET_ADDRSTRLEN
+#include <ctype.h> // This is required for isdigit
 
 // Function defintions
 void valid_port_number_checker(char*);
-
-
-
 
 int main() {
 
@@ -19,10 +18,11 @@ char server_mode[] = "Stream";
 char server_file_structure[] = "File";
 char server_representation_type[] = "ASCII Non-print";
 
-char server_message [256];
-snprintf(server_message, sizeof(server_message), "Welcome to the FTP server.\nServer is in '%s' mode, using structure type '%s', and using '%s' representation type.\n\n", server_mode, server_file_structure, server_representation_type);
+char server_side_welcome_message [256];
+snprintf(server_side_welcome_message, sizeof(server_side_welcome_message), "Welcome to the FTP server.\nServer is in '%s' mode, using structure type '%s', and using '%s' representation type.\n\n", server_mode, server_file_structure, server_representation_type);
 
-printf("%s", server_message);
+
+printf("%s", server_side_welcome_message);
 printf("What port would you like the server to listen on? 21 is standard.\n");
 
 char command_port_number[6];
@@ -32,9 +32,9 @@ while (getchar() != '\n'); // This clears the input buffer, so the next scanf do
 
 
 
-printf("Port selected was %s.\n", command_port_number); 
+printf("\nPort selected was %s.\n", command_port_number); 
 printf("Is this the correct port number? y/n\n");
-char port_check[2];
+char port_check[1];
 scanf("%c", port_check);
 
 if (port_check[0] != 'y' && port_check[0] != 'Y') {
@@ -46,45 +46,130 @@ if (port_check[0] != 'y' && port_check[0] != 'Y') {
 // Running a function to check the port number inputted is valid.
 valid_port_number_checker(command_port_number);
 
-
 // Creating a two socket (Control and Data).
 // AF_INET = IPv4, SOCK_STREAM = TCP, 0 = IP
 
 // Control channel socket created
-int server_control_socket = socket(AF_INET, SOCK_STREAM, 0);
-if (server_control_socket < 0) {
-    perror("Error in creating control socket");
+int server_control_listener = socket(AF_INET, SOCK_STREAM, 0);
+if (server_control_listener < 0) {
+    perror("Error in creating control socket listener.\n");
     exit(EXIT_FAILURE);
 }
 
 
 /* Instert some code here to create data sock later - should likely be in its own function. */
 
+// Defining the socket structure for the control channel
+struct sockaddr_in server_address_control_port;
+server_address_control_port.sin_family = AF_INET;
+server_address_control_port.sin_port = htons((uint16_t)atoi(command_port_number)); // Convert port number to integer
+server_address_control_port.sin_addr.s_addr = INADDR_ANY; // This means the server will listen on all interfaces.
+
+// Binding the control channel to the socket
+if (bind(server_control_listener, (struct sockaddr*) &server_address_control_port, sizeof(server_address_control_port)) < 0) {
+    perror("Error in binding control socket");
+    exit(EXIT_FAILURE);
+}
+
+// Listening on the control channel
+if (listen(server_control_listener, 5) < 0) {
+    perror("Error with listen() on control socket.");
+    exit(EXIT_FAILURE);
+}
+
+// Accepting a client connection, maybe this needs to be made into a WHILE loop?
+int client_control_connection_socket = -1;
+int accept_fail_count = 0;
+
+// Creating a structure to save the details of the client connecting to server.
+struct sockaddr_in client_address;
+int client_address_length = sizeof(client_address); // need to stop buffer overflows
+
+printf("\nWaiting for client connection...\n");
+while (client_control_connection_socket == -1) { // When client_connection_socket is successful it will become the next non-negative value for the FD (file descriptor) on the system
+    client_control_connection_socket = accept(server_control_listener, (struct sockaddr *)&client_address, &client_address_length); 
+    // Last two arguments of accept are us storing the incoming connection data (IP, port etc). We also needed to cast client_address as accept() expected sockaddr structure not sockaddr_in
+    // accept() is known as a BLOCKING function, meaning it will wait until a client connects to the server before another itiration of the while loop starts.
+
+    accept_fail_count += 1;
+
+    if (client_control_connection_socket < 0) {
+        perror("Error accepting connection from client.\n");
+    }
+
+    if (accept_fail_count > 3) {
+        perror("Failed to accept client connection three times in a row.\n");
+        exit(EXIT_FAILURE);
+    }
+
+accept_fail_count = 0; // Resetting the fail count for the next iteration of the while loop. <-- Good practice, but pointless until the above is inside its own loop
+} // End of while client_control_connection_socket loop
 
 
-// struct sockaddr_in server_address_control;
-// server_address_control.sin_family = AF_INET;
-// server_address_control.sin_port = htons((uint16_t)atoi(command_port_number)); // Convert port number to integer
-// server_address_control.sin_addr.s_addr = INADDR_ANY; // This means the server will listen on all interfaces.
-// server_address_control.sin_addr.s_addr = INADDR_ANY; // This means the server will listen on all interfaces.
+// Send initial message to client, 220 means server is ready, 120 means server asks for client to wait.
+// Currently, dont think I need to send a 120 message.
+char welcome_client_message[256];
+snprintf(welcome_client_message, sizeof(welcome_client_message), "220 Welcome to the FTP server!\r\n");
 
-// // Defining the socket structure for the data channel
-// struct sockaddr_in server_address_data;
-// server_address_data.sin_family = AF_INET;
-// // Port number is not set, as this is negotiated depending on the client's request.
-// server_address_data.sin_addr.s_addr = server_address_control.sin_addr.s_addr; // Match the interface of the control channel.
+if (send(client_control_connection_socket, welcome_client_message, sizeof(welcome_client_message), 0) < 0) {
+    perror("Error sending welcome message to client\n");
+    exit(EXIT_FAILURE);
+}
 
+// Display IP of client connected to control socket
+char client_ip[INET_ADDRSTRLEN]; //INET_ADDRSTRLEN is a constant from arpa/inet.h built specifically to represent the length of IPv4 address, and the null terminator.
+inet_ntop(AF_INET, &client_address.sin_addr, client_ip, INET_ADDRSTRLEN); // converts the IP address from binary to text form.
+
+printf("Client connected from IP address: %s\n", client_ip);
+
+// Close listerner socket
+if (close(server_control_listener) != 0) {
+    perror("Error closing control socket");
+    exit(EXIT_FAILURE);
+} else {
+    printf("Command channel has closed.\n");
+    printf("\n");
+    printf("Goodbye.\n");
+}
+
+// Closer the control socket connected to client
+if (close(client_control_connection_socket) != 0) {
+    perror("Error closing control socket");
+    exit(EXIT_FAILURE);
+} else {
+    printf("Command channel has closed.\n");
+    printf("\n");
+    printf("Goodbye.\n");
+}
 
 return 0;
-}
+} // end of main
+
+
 
 // A function to check if the port number is valid
 void valid_port_number_checker(char* char_port) {
     
+    // Checking char_port is not empty.
+    if (char_port[0] == '\0') {
+        printf("\n");
+        printf("Please enter a port number.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Checking char_port is a number
+    for (int i = 0; char_port[i] != '\0'; i++) {
+        if (!isdigit(char_port[i])) {
+            printf("\n");
+            printf("Invalid port number '%s'. Port must be a number.\n", char_port);
+            exit(5);
+        }
+    }
+
     // Turning char_port into an int
     int int_port = atoi(char_port);
-    if (int_port < 0 || int_port > 65535) {
-        printf("/n");
+    if (int_port < 1 || int_port > 65535) {
+        printf("\n");
         printf("Invalid port number '%s'. Port must be between 0 and 65535.\n", char_port);
         exit(5);
     }
